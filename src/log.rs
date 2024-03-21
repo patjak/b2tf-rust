5ad9f1f0ab0ca5efa6209490c6d612aa6003b1aa
Patrik Jakobsson <pjakobsson@suse.de>
2024-03-21 12:49:53 +0100

    Implement struct Log and Log::load()
    Signed-off-by: Patrik Jakobsson <pjakobsson@suse.de>

---
+++ src/log.rs
@@ -0,0 +1,25 @@
+#![allow(unused)]
+use std::fs;
+use std::error::Error;
+
+pub struct Log {
+    pub filename: String,
+    pub config: String,
+    pub commits: String,
+}
+
+impl Log {
+    pub fn load(&mut self) -> Result<(), Box<dyn Error>>  {
+        let contents: String = fs::read_to_string(&self.filename)?.parse()?;
+        let slices: Vec<&str> = contents.split("\n---\n").collect();
+
+        self.config = String::from(slices[0]);
+        self.commits = String::from(slices[1]);
+
+        if slices.len() != 2 {
+            return Err("Log::Load() Invalid format".into());
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+mod log;
+use crate::log::*;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut log = Log {
+        filename: String::from("commits.log"),
+        config: String::from(""),
+        commits: String::from("")
+    };
+
+    log.load()?;
+
+    Ok(())
 }
