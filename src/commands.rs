cb963a3007ce38c14862380089e51af1d9003ba6
Patrik Jakobsson <pjakobsson@suse.de>
2025-04-25 15:59:20 +0200

    commands: use the Git::cmd() function in cmd_populate()
    Signed-off-by: Patrik Jakobsson <pjakobsson@suse.de>

--- src/commands.rs
+++ src/commands.rs
@@ -14,19 +14,8 @@ pub fn cmd_populate(options: &Options, log: &mut Log) -> Result<(), Box<dyn Erro
     let range_stop = options.range_stop.clone().unwrap();
     let paths = options.paths.clone().unwrap();
 
-    let query = format!("git -C {git_dir} rev-list --reverse --topo-order --no-merges --oneline --no-abbrev-commit {range_start}..{range_stop} -- {paths}");
-
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(&query)
-        .output()
-        .expect(format!("Failed to execute: {}\n", &query).as_str());
-
-    if !output.status.success() {
-        return Err("Failed to execute git rev-list".into());
-    }
-
-    let stdout = String::from_utf8(output.stdout).expect("Invalid UTF8");
+    let query = format!("rev-list --reverse --topo-order --no-merges --oneline --no-abbrev-commit {range_start}..{range_stop} -- {paths}");
+    let stdout = Git::cmd(query, &git_dir)?;
     let lines: Vec<&str> = stdout.split("\n").collect();
 
     let mut commits = String::from("");
